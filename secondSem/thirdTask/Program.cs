10492f6788597abcfb13a00611f17ce8dc5edeea
Dmitriy <dmitr.wischnewski@mail.ru>
2023-02-10 08:51:48 +0300

    Thitd task is ready

---
+++ secondSem/thirdTask/Program.cs
@@ -0,0 +1,29 @@
+﻿/*Напишите программу, которая принимает на вход цифру, обозначающую день недели,
+и проверяет, является ли этот день выходным.
+
+6 -> да
+7 -> да
+1 -> нет*/
+
+
+Console.Write("Введите цифру, соответствующую дню недели: ");
+int digit = int.Parse(Console.ReadLine()!);
+
+if (digit > 7 || digit < 1 )
+{
+    Console.Write("Дней в неделе семь... ");
+}
+
+    switch (digit)
+{
+    case 6:
+       Console.WriteLine("Да");
+       break;
+    case 7:
+       Console.WriteLine("Да");
+       break;    
+    default:
+       Console.WriteLine("Нет");
+       break;
+}
+
