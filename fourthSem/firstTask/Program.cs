7e74b29a0aa41614a82a6c5627b52795856d479b
Dmitriy <dmitr.wischnewski@mail.ru>
2023-02-14 11:31:03 +0300

    First task is ready

---
+++ fourthSem/firstTask/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите функцию, которая принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// Нельзя использовать библиотеку Math!
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+
+double MyMathPow (double arg1, double arg2)
+{   
+    double result = 1;
+    for (double i = 1; i <= arg2; i++)
+    {        
+        result *= arg1;
+    }      
+    return result;
+}
+
+Console.Write("Введите любое число для возведения его в степень: ");
+int value = int.Parse(Console.ReadLine()!);
+Console.Write("Введите любое число, котторое будет соответствовать степени возведения: ");
+int powValue = int.Parse(Console.ReadLine()!);
+int result = Convert.ToInt32(MyMathPow(value, powValue));
+Console.Write($"Результат: {result}");
