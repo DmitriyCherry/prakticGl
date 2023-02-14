5f8184cda81284a5ec24e7c75817e62e54c6016f
Dmitriy <dmitr.wischnewski@mail.ru>
2023-02-14 12:48:43 +0300

    Second task is ready

---
+++ fourthSem/secondTask/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе:
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+int SumDigits (int arg)
+{
+    int sum = 0;
+    int forIf = arg /10;
+    if (forIf == 0)
+    {
+        sum = arg;
+    }
+    
+    for (int i = 0; forIf > 0; i++)
+    {               
+        arg = arg % 10;         
+        sum += arg + (forIf % 10);
+        forIf /= 10;
+        arg /= 10;       
+    }
+    return sum;
+}
+
+Console.Write("Введите любое целое число: ");
+int count = int.Parse(Console.ReadLine()!);
+int result = SumDigits(count);
+Console.Write($"Результат работы функции: {result}");
