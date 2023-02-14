0bb48a33534e2abf6147e64f96c2899b68cd2736
Dmitriy <dmitr.wischnewski@mail.ru>
2023-02-14 13:17:24 +0300

    Third task is ready

---
+++ fourthSem/thirdTask/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+
+void MyArray (int[] array)
+{    
+    for (int i = 0; i < array.Length; i++)
+    {
+        string thing = ", ";
+        array[i] = new Random().Next(0, 100);
+        if (i == array.Length - 1)
+        {
+            thing = "";
+        }
+        Console.Write(array[i] + thing);
+    }
+}
+
+Console.Write("Введите количество элементов в новом массиве: ");
+int elements = int.Parse(Console.ReadLine()!);
+int[] newArray = new int [elements];
+MyArray(newArray); 
