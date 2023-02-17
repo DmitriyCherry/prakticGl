b455e8c9779db2a0ec38731bec094ac1312c5ea0
Dmitriy <dmitr.wischnewski@mail.ru>
2023-02-17 12:36:37 +0300

    Second task is ready

--- fifthSem/secondTask/Program.cs
+++ fifthSem/secondTask/Program.cs
@@ -2,3 +2,41 @@
 // стоящих на нечётных позициях.
 // [3, 7, 23, 12] -> 19
 // [-4, -6, 89, 6] -> 0
+
+
+int SumNegElmts (int[] arg)
+{
+    int sumNegative = 0;
+    for (int i = 0; i < arg.Length; i++)
+    {
+        if (arg[i] % 2 != 0)
+        {
+            sumNegative += arg[i];
+        }
+    }
+    return sumNegative;
+}   
+
+Console.Write("Введите количество эелментов нового массива: ");
+int elements = int.Parse(Console.ReadLine()!);
+int[] myArray = new int [elements];
+for (int i = 0; i < elements; i++)
+{
+    myArray[i] = new Random().Next(0, 1000);
+    string thing = ", ";    
+    if (i == 0)
+        {
+            Console.Write("[" + myArray[i] + thing);
+        }else
+        {
+            if (i == elements - 1)
+            {
+                Console.Write(myArray[i] + "]");
+            }else
+        {
+            Console.Write(myArray[i] + thing);
+        }
+        }            
+}
+int result = SumNegElmts(myArray);
+Console.Write($"--> {result}");
