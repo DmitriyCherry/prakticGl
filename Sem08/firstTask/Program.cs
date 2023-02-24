0fec61a83b202622a0d7b51e6fc05080dca327a5
Dmitriy <dmitr.wischnewski@mail.ru>
2023-02-24 11:34:13 +0300

    Second task is ready

---
+++ Sem08/firstTask/Program.cs
@@ -0,0 +1,52 @@
+﻿/* Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы
+каждой строки двумерного массива. Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4    В итоге получается вот такой массив:
+                         7 4 2 1
+                         9 5 3 2
+                         8 4 4 2*/
+
+void PrintArray(int[,] arg)
+{
+int height = arg.GetLength(0);
+int widht = arg.GetLength(1);
+
+for (int i = 0; i < height; i++)
+{
+    for (int j = 0; j < widht; j++)
+    {       
+        Console.Write(arg[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+}
+
+void RevMinOrMax (int[,] arg)
+{    
+for (int i = 0; i < arg.GetLength(0); i++)
+    {     
+        
+        for (int j = 0; j < arg.GetLength(1); j++)
+        {                
+          int max = arg[0,0];
+            if (arg[i,j] > max)
+            {
+                int temp = max;
+                max = arg[i,j];
+             }            
+            
+                arg[i,j] = temp; 
+        }
+   }
+}
+
+int[,] myArray = new int [,]
+{
+{1, 4, 7, 2},
+{5, 9, 2, 3},
+{8, 4, 2, 4}  
+};
+
+RevMinOrMax(myArray);
+PrintArray(myArray);
---
+++ Sem08/secondTask/Program.cs
@@ -0,0 +1,68 @@
+﻿/*Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей 
+суммой элементов.
+Например, задан массив:
+
+1 4 7 2
+5 9 2 3
+8 4 2 4
+5 2 6 7
+
+Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой
+элементов: 1 строка*/
+
+void PrintArray(int[,] arg)
+{
+int height = arg.GetLength(0);
+int widht = arg.GetLength(1);
+
+for (int i = 0; i < height; i++)
+{
+    for (int j = 0; j < widht; j++)
+    {       
+        arg[i,j] = new Random().Next(0,10);
+        Console.Write(arg[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+}
+
+void SumRows(int[,] arg)
+{    
+    int sumRow = 0;        
+    int minSum = 0;
+    int result = 0;    
+    for (int i = 0; i < arg.GetLength(0); i++)
+    {         
+        for (int j = 0; j < arg.GetLength(1); j++)
+        {            
+            sumRow += arg[i,j];     
+        }
+        if (minSum < sumRow)
+        {
+            minSum = sumRow;
+        }       
+        sumRow = 0;        
+    }
+    for (int i = 0; i < arg.GetLength(0); i++)
+    {
+        for (int j = 0; j < arg.GetLength(1); j++)
+        {
+            sumRow+= arg[i,j];
+        }
+        if (minSum > sumRow)
+        {
+            minSum = sumRow;
+            result = minSum + i;
+        }
+        sumRow = 0;        
+    }
+    Console.WriteLine($"Номер строки с минимальной суммой элементов --> {result - minSum + 1}-ая строка.");
+}
+
+Console.Write("Введите количество строк: ");
+int height = int.Parse(Console.ReadLine()!);
+Console.Write("Введите количество столбцов: ");
+int widht = int.Parse(Console.ReadLine()!);
+int[,] myArray = new int [height, widht];
+PrintArray(myArray);
+SumRows(myArray);
